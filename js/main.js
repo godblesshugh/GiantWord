da1207efeda5f808640ca76a6d7607bbcf9a1079
godblesshugh <godblesshugh@outlook.com>
2016-08-22 18:08:59 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,51 @@
+function strToImg(strElement, imgElement, canvasElement, fontColor, callback){
+    var str = document.getElementById(strElement).value.replaceAll("\r\n", "").replaceAll("\n", "")
+    var strList = []
+    var basicFont = "px Courier New"
+    var fontIndex = 0
+    var lineNum = 4
+    if (str.length > lineFont[2][0]) {
+        str = str.substr(0, 39)
+    }
+    if (str.length <= lineFont[0][0]) {
+        strList.push(str)
+        fontIndex = 0
+    }
+    if (str.length > lineFont[0][0] && str.length <= lineFont[1][0]) {
+        lineNum = lineFont[1][0] / 2
+        var strLength = str.length
+        for (var i = 0; i < 2 && strLength > 0; i++) {
+            strLength -= lineNum
+            strList.push(str.substr(lineNum * i, lineNum))
+        }
+        fontIndex = 1
+    }
+    if (str.length > lineFont[1][0] && str.length <= lineFont[2][0]) {
+        lineNum = lineFont[2][0] / 3
+        var strLength = str.length
+        for (var i = 0; i < 3 && strLength > 0; i++) {
+            strLength -= lineNum
+            strList.push(str.substr(lineNum * i, lineNum))
+        }
+        fontIndex = 2
+    }
+    var canvas = document.getElementById(canvasElement)
+    var context = canvas.getContext("2d")
+    // context.textAlign='center';
+    context.save()
+    context.fillStyle='#FFFFFF';
+    context.fillRect(0,0,canvas.width,canvas.height);
+    context.restore()
+
+    context.fillStyle = fontColor;
+    context.font = lineFont[fontIndex][1] + "px Courier New"
+    for (var i = 0; i < strList.length; i++) {
+        context.fillText(strList[i], 0, (i + 1) * lineFont[fontIndex][1]);
+    }
+    var save_img = document.getElementById(imgElement)
+    save_img.src = canvas.toDataURL("image/gif")
+    save_img.width = screenWidth * 0.8
+    save_img.onload = function(){
+        callback()
+    }
+}
---
+++ js/my-app.js
@@ -0,0 +1,14 @@
+// Initialize app and store it to myApp variable for futher access to its methods
+var myApp = new Framework7();
+
+// We need to use custom DOM library, let's save it to $$ variable:
+var $$ = Framework7.$;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  // dynamicNavbar: true
+});
+
+// Now we need to run the code that will be executed only for About page.
+// For this case we need to add event listener for "pageInit" event
